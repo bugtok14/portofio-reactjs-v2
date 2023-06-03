076777148c44a15bcafa46b475a009d9de93cdda
bugtok14 <davemarapao14@gmail.com>
2023-06-03 20:16:24 +0800

    june update7

--- src/components/HeroImg.js
+++ src/components/HeroImg.js
@@ -2,6 +2,7 @@ import React from 'react'
 import './HeroImg.css'
 import  IntroImg from '../assests/background1.png'
 import { Link } from 'react-router-dom'
+import Typical from 'react-typical'
 
 function HeroImg() {
   return (
@@ -10,7 +11,20 @@ function HeroImg() {
             <img className='intro-img' src={IntroImg} alt="IntroImage"/>
         </div>    
         <div className='content'>
-            <p>Hi, I'M DAVE MARAPAO</p>
+            <p>Hi, I'm <Typical 
+                           
+                           steps={[
+                                " Dave Marapao 👨‍💻",
+                                3000,
+                                " a Developer 💻",
+                                3000,
+                                " a Tester 🧪",
+                                3000
+                           ]}
+                           loop={Infinity}
+                           wrapper="i"
+                    />
+            </p>
             <h1>Software Tester and Developer</h1>
             <div>
                 <a href='https://drive.google.com/file/d/1NpN7xM-v2oU7EKd0BvDejz742roXjbWD/view?usp=sharing' className='btn'>My Resume</a>
